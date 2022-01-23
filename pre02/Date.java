df5c735a0ac6d64e131795e382a976c2a313a616
Christine Marra <96950263+chrismarra21@users.noreply.github.com>
2022-01-23 21:04:18 +0000

    completed Time.java

--- pre02/Date.java
+++ pre02/Date.java
@@ -1,14 +1,14 @@
 import java.io.*;
  class Date {
     public static void main(String[] args){
-       string day = Monday;
-       string month = February;
+       String day = "Monday";
+       String month = "February";
        int date = 17;
        int year = 1969;
         
        
         
         
-        System.out.println(day+", "+ month + date ", "+ year);
+        System.out.println(day +", "+ month + " " + date + ", "+ year);
     }
 }
