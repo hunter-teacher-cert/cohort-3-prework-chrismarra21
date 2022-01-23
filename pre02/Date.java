dbbd801f4238b28cb3d63a79e0da587447b4703a
Christine Marra <96950263+chrismarra21@users.noreply.github.com>
2022-01-23 18:54:08 +0000

    added file Date.java

---
+++ pre02/Date.java
@@ -0,0 +1,14 @@
+import java.io.*;
+ class Date {
+    public static void main(String[] args){
+       string day = Monday;
+       string month = February;
+       int date = 17;
+       int year = 1969;
+        
+       
+        
+        
+        System.out.println(day+", "+ month + date ", "+ year);
+    }
+}
