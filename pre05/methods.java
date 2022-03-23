a6b22636bb8f5d9c025aa81964197d5cf7192bcf
Christine Marra <96950263+chrismarra21@users.noreply.github.com>
2022-03-23 17:57:28 +0000

    Added folder pre05 and added a file called methods.

--- pre05/methods.java
+++ pre05/methods.java
@@ -4,16 +4,44 @@ public class methods
 {
    public static void main(String[] args)
    {
-       Divisible(9,3);
+       
+     System.out.println(isdivisible(10,3));
+     System.out.println(istriangle(4,5,6));
+     System.out.println(ack(3,3));
    }
 
  // 6.2 put in Method.java
 
-  public static boolean Divisible(int m,int n )
+  public static boolean isdivisible(int m,int n )
     {
-        if((m % n) == 0) 
-        return true;
-        else
-        return false;
+        return (m % n==0);
     }
+  public static boolean istriangle(int a,int b,int c)
+    {
+        if((a+b<c)||(a+c<b)||(c+b<a)) 
+          return false;
+        else
+          return true;
+    }
+
+  public static int ack(int m, int n)
+    {
+        if (m == 0){
+          return n+1;
+
+          }else if (m > 0) && (n == 0){
+            return ack(m-1, 1);
+          }else {     
+            return ack(m-1, ack(m, n-1));
+        }    
+
+       }
+
+
+
+
+  
 }
+
+
+
