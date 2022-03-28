648e91bf1302bd61eed7da330447707a4ed0d8a2
Christine Marra <96950263+chrismarra21@users.noreply.github.com>
2022-03-28 23:22:18 +0000

    fixed methods code

--- pre05/methods.java
+++ pre05/methods.java
@@ -29,7 +29,7 @@ public class methods
         if (m == 0){
           return n+1;
 
-          }else if (m > 0) && (n == 0){
+          }else if ((m > 0) && (n == 0)){
             return ack(m-1, 1);
           }else {     
             return ack(m-1, ack(m, n-1));
